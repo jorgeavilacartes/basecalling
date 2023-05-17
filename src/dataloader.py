fabed203b47f36ed1ccb01db42d956dd30f9aa96
Jorge <jorgeavilacartes@gmail.com>
2023-05-17 19:45:56 +0200

    fix dataloader and training step. Add tqdm to environment

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,13 +1,22 @@
 import numpy as np
 import h5py
+import torch
+
 from pathlib import Path
 from typing import Union
 from torch.utils.data import Dataset
 
 class DatasetONT(Dataset):
-
-    def __init__(self, recfile: Union[str,Path],): # seq_len: int = 4096):
+    """Load a single sample for training/evaluating the network
+    """    
+    def __init__(self, recfile: Union[str,Path], output_network_len: int):
+        """
+        Args:
+            recfile (Union[str,Path]): path to hdf5 file with events and labels
+            output_network_len (int): length of the output signal of the networkssss (required for CTC loss)
+        """        
         self.recfile = recfile # hdf5 file with training data
+        self.output_network_len = output_network_len
 
         # load metadata recfile
         h5 = h5py.File(self.recfile, "r")
@@ -16,7 +25,8 @@ class DatasetONT(Dataset):
         self.metadata = dict(
             n_signal   = n_signals,  # number of signals in the file (called 'events' in the files from RODAN)
             signal_len = signal_len, # length of the signal (or event length in RODAN)
-            label_len  = label_len   # length of the labels; each position correspond to one character in {-,A,C,G,T}
+            label_len  = label_len,   # length of the labels; each position correspond to one character in {-,A,C,G,T}
+            output_network_len = output_network_len
         )
         h5.close() 
 
@@ -29,7 +39,15 @@ class DatasetONT(Dataset):
         h5.close()
 
         # include channel to signal [Channel, Signal]
-        return np.expand_dims(signal,axis=0), label
+        signals = torch.from_numpy(np.expand_dims(signal,axis=0))
+        labels = torch.from_numpy(label)
+
+        # w.r.t the loss function
+        target_lens = torch.from_numpy(np.array(self.metadata["label_len"])) 
+        input_lens = torch.from_numpy(np.array(self.metadata["output_network_len"]))
+       
+        return signals, labels, input_lens, target_lens
+        # return torch.from_numpy(np.expand_dims(signal,axis=0)), torch.from_numpy(label),
         
     def __len__(self,):
         "number of pairs (signal, label) in the input file"
