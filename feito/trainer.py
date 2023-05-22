e801181ef10e7842237fb10ab5cfe52559db940f
Jorge <jorgeavilacartes@gmail.com>
2023-05-22 11:54:59 +0200

    validation step included in Trainer

--- feito/trainer.py
+++ feito/trainer.py
@@ -1,4 +1,5 @@
 # Trainer class was inspired from https://github.com/nanoporetech/bonito/blob/master/bonito/training.py
+import torch
 from tqdm import tqdm
 from typing import Optional, Callable, List
 
@@ -19,8 +20,8 @@ class BasecallerTrainer:
     def fit(self, epochs):
         for t in range(epochs):
             epoch = t+1
-            self.train_one_epoch(epoch)
-            # self.validate_one_epoch(epoch)
+            train_losses = self.train_one_epoch(epoch)
+            val_losses   = self.validate_one_epoch(epoch)
         print("Done!")
 
 
@@ -55,10 +56,8 @@ class BasecallerTrainer:
             for n_batch, batch in enumerate(self.train_loader):
 
                 # Description for progress bar
-                if epoch:
-                    progress_bar.set_description(f"Epoch: {epoch} | batch: {n_batch}/{n_batches}")
-                else:
-                    progress_bar.set_description(f"batch {n_batch}/{n_batches}")
+                progress_bar.set_description(f"Training Epoch: {epoch} | Batch: {n_batch}/{n_batches}")
+
 
                 losses=self.train_one_batch(batch)
 
@@ -66,18 +65,46 @@ class BasecallerTrainer:
                 progress_bar.update(1)
 
             # TODO: add callbacks
-
+        
+        return losses 
+    
     # Validation
-    def validate_one_batch(self,):
+    def validate_one_batch(self, batch):
         # TODO: validate one batch
         # https://github.com/nanoporetech/bonito/blob/655feea4bca17feb77957c7f8be5077502292bcf/bonito/training.py#L185
-        pass
-
-    def validate_one_epoch(self,):
+        self.model.eval()
+        X, y, output_len, target_len = (x.to(self.device) for x in batch)
+        preds  = self.model(X)
+        losses = self.criterion(preds, y, output_len, target_len)
+        
+        return losses  
+        
+    def validate_one_epoch(self, epoch):
         # TODO: validate epoch
         # https://github.com/nanoporetech/bonito/blob/655feea4bca17feb77957c7f8be5077502292bcf/bonito/training.py#L206
-        pass
+        self.model.eval()
+        n_batches=len(self.train_loader)
 
+        losses = []
+
+        with tqdm(total=n_batches, leave=True, ncols=100, bar_format='{l_bar}{bar}| [{elapsed}{postfix}]') as progress_bar:
+            
+            with torch.no_grad():
+                
+                for n_batch, batch in enumerate(self.validation_loader):
+
+                    # Description for progress bar
+                    progress_bar.set_description(f"Validation Epoch: {epoch} | Batch: {n_batch}/{n_batches}")
+
+                    # compute loss function for the batch
+                    loss = self.validate_one_batch(batch)
+                    losses.append(loss.item())        
+
+                    progress_bar.set_postfix(loss='%.4f' % loss)
+                    progress_bar.update(1)
+        
+        return torch.Tensor(losses).mean() 
+            
 
     # to consider accuracies of alinged reads in the validation step 
     # https://github.com/nanoporetech/bonito/blob/655feea4bca17feb77957c7f8be5077502292bcf/bonito/util.py#L354
---
+++ feito/utils/accuracy.py
@@ -0,0 +1,64 @@
+# From Bonito
+# https://github.com/nanoporetech/bonito/blob/655feea4bca17feb77957c7f8be5077502292bcf/bonito/util.py
+
+import re
+import parasail
+from collections import defaultdict
+
+split_cigar = re.compile(r"(?P<len>\d+)(?P<op>\D+)")
+
+def accuracy(ref, seq, balanced=False, min_coverage=0.0):
+    """
+    Calculate the accuracy between `ref` and `seq`
+    """
+    alignment = parasail.sw_trace_striped_32(seq, ref, 8, 4, parasail.dnafull)
+    counts = defaultdict(int)
+
+    q_coverage = len(alignment.traceback.query) / len(seq)
+    r_coverage = len(alignment.traceback.ref) / len(ref)
+
+    if r_coverage < min_coverage:
+        return 0.0
+
+    _, cigar = parasail_to_sam(alignment, seq)
+
+    for count, op  in re.findall(split_cigar, cigar):
+        counts[op] += int(count)
+
+    if balanced:
+        accuracy = (counts['='] - counts['I']) / (counts['='] + counts['X'] + counts['D'])
+    else:
+        accuracy = counts['='] / (counts['='] + counts['I'] + counts['X'] + counts['D'])
+    return accuracy * 100
+
+def parasail_to_sam(result, seq):
+    """
+    Extract reference start and sam compatible cigar string.
+
+    :param result: parasail alignment result.
+    :param seq: query sequence.
+
+    :returns: reference start coordinate, cigar string.
+    """
+    cigstr = result.cigar.decode.decode()
+    first = re.search(split_cigar, cigstr)
+
+    first_count, first_op = first.groups()
+    prefix = first.group()
+    rstart = result.cigar.beg_ref
+    cliplen = result.cigar.beg_query
+
+    clip = '' if cliplen == 0 else '{}S'.format(cliplen)
+    if first_op == 'I':
+        pre = '{}S'.format(int(first_count) + cliplen)
+    elif first_op == 'D':
+        pre = clip
+        rstart = int(first_count)
+    else:
+        pre = '{}{}'.format(clip, prefix)
+
+    mid = cigstr[len(prefix):]
+    end_clip = len(seq) - result.end_query - 1
+    suf = '{}S'.format(end_clip) if end_clip > 0 else ''
+    new_cigstr = ''.join((pre, mid, suf))
+    return rstart, new_cigstr
