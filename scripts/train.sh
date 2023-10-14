bfa93f6417da9f07ab02d02d872f1e4a7adb5a27
Jorge <jorgeavilacartes@gmail.com>
2023-10-14 08:37:28 +0200

    train with gaussian noise

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 OUTDIR="output-rodan-smoothctc-ranger-gnoise"
 
-# --path-train data/RODAN/train/rna-train.hdf5 \
-# --path-val data/RODAN/train/rna-valid.hdf5 \
-python feito/train.py \
---path-train data/subsample_train.hdf5 \
---path-val data/subsample_val.hdf5 \
+mkdir -p $OUTDIR/logs
+
+# --path-train data/subsample_train.hdf5 \
+# --path-val data/subsample_val.hdf5 \
+
+/usr/bin/time -v python feito/train.py \
+--path-train data/RODAN/train/rna-train.hdf5 \
+--path-val data/RODAN/train/rna-valid.hdf5 \
 --epochs 15 \
 --batch-size 32 \
 --num-workers 4 \
---augmentation gaussian_noise flip \
+--augmentation gaussian_noise \
 --model Rodan \
---device cpu \
+--device cuda \
 --path-weights output-rodan-smoothctc-momentum/training/checkpoints/Rodan-epoch30.pt \
 --outfile-train-logger $OUTDIR/training/metrics.csv \
---dirpath-checkpoint $OUTDIR/training/checkpoints
+--dirpath-checkpoint $OUTDIR/training/checkpoints > $OUTDIR/logs/train.std.log 2> $OUTDIR/logs/train.err.log
